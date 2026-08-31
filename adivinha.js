7685c37ae7030824af897a27d67f0883864e51e4
Brenda <basl1@aluno.ifal.edu.br>
2026-08-31 20:21:54 -0300

    1.0

--- adivinha.js
+++ adivinha.js
@@ -1,18 +1,42 @@
+//declarar o botão como uma constante;
 const botaoInicio = document.getElementById('botaoInicio');
 
+//clicar no botão para iniciar o jogo;
 botaoInicio.addEventListener('click', function(){
 
-    const secretNumber = Math.floor(Math.random()*100);
+    //Gera um número aleatorio entre 1 e 100
+    const secretNumber = Math.floor(Math.random()*100) +1 ;
 
-    let usuario = parseInt(prompt("Digite um número de 0  a 100: "));
+    let acerto = false;
+    //controla a quantidade de tentativas de usuario
+    for(let tentativas = 1; tentativas<=7; tentativas++){
 
-    if(secretNumber > usuario){
-        alert("O seu palpite é menor que o número secreto!");
+        let inputUsuario = prompt('Tentativa ${tentativas} de 7:\nDigite seu palpite(entre 1 e 100): ');
+    
+        //verifica se há um numero digitado.
+    if(inputUsuario === null){
+        alert("Jogo cancelado!");
         return;
-    } else if (secretNumber < usuario){
-        alert("O seu palpite é maior que o número secreto!");
-        return;
+    } 
+    //verifica se o numero digitado é valido
+    let usuario = parseInt(inputUsuario);
+    
+    //é o mesmo que "Se o numero digitado pelo usuario for igual ao numero secreto"
+    if (usuario === secretNumber){
+        alert("Parabéns, você acertou!!");
+        acertou = true;
+        break;
+    } 
+    //é o mesmo que "Senão, se o numero digitado pelo usuario for maior que o numero secreto"
+    else if (usuario < secretNumber) {
+        alert("O número secreto é maior!");
+
+        //é o mesmo que "Senão, se o numero digitado pelo usuario for menor que o numero secreto"
+    } else if(usuario > secretNumber){
+        alert("O número secreto é menor!");
+        //é o mesmo que "Se for nenhuma das anteriores..."
     } else {
-        alert("Você acertou!!");
+        alert("Por favor, digite um número!");
+        tentativas--;
     }
-    });
+}});
